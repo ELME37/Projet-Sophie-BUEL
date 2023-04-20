9a965949086511add6c300804fe0c819469ac12a
Mickael TURQUAIS <mickael.turquais@laposte.net>
2023-04-20 11:16:07 +0200

    Tri par catégorie

--- FrontEnd/assets/scripts.js
+++ FrontEnd/assets/scripts.js
@@ -16,3 +16,69 @@ fetch("http://localhost:5678/api/works")
         }
     })
 
+const category = document.getElementById('category')
+
+
+fetch("http://localhost:5678/api/categories")
+    .then((Response) => Response.json())
+    .then((dataCategory) => {
+
+        const buttonAll = document.createElement('button')
+        buttonAll.classList.add('button_all')
+        buttonAll.innerHTML = `<span>Tous</span>`
+
+        buttonAll.addEventListener('click', () => {
+            gallery.innerHTML = "";
+
+            fetch("http://localhost:5678/api/works")
+                .then((Response) => Response.json())
+                .then((dataGallery) => {
+
+                    for (let i = 0; i < dataGallery.length; i++) {
+                        const article = document.createElement('article')
+                        article.innerHTML = `
+                    <img src="${dataGallery[i].imageUrl}" alt="${dataGallery[i].title}">
+                    <h3>${dataGallery[i].title}</h3>`
+
+                        gallery.appendChild(article)
+
+                    }
+                })
+
+        })
+
+        category.appendChild(buttonAll)
+
+        for (let j = 0; j < dataCategory.length; j++) {
+
+            const button = document.createElement('button')
+            button.classList.add('button_category')
+            button.innerHTML = `
+            <span>${dataCategory[j].name}</span>`
+
+            button.addEventListener('click', () => {
+                gallery.innerHTML = "";
+
+                fetch("http://localhost:5678/api/works")
+                    .then((Response) => Response.json())
+                    .then((dataGallery) => {
+                        for (let i = 0; i < dataGallery.length; i++) {
+
+                            if (dataGallery[i].categoryId === dataCategory[j].id) {
+                                const article = document.createElement('article')
+                                article.innerHTML = `
+                                <img src="${dataGallery[i].imageUrl}" alt="${dataGallery[i].title}">
+                                <h3>${dataGallery[i].title}</h3>`
+
+                                gallery.appendChild(article)
+                            }
+                        }
+                    })
+            })
+
+
+
+            category.appendChild(button)
+        }
+    }
+    )
