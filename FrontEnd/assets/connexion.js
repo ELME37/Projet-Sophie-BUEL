cd8509dc1fe972c5727fcfb0910fffa4dce05517
Mickael TURQUAIS <mickael.turquais@laposte.net>
2023-05-15 16:51:19 +0200

    Ajout et suppression travaux

--- FrontEnd/assets/connexion.js
+++ FrontEnd/assets/connexion.js
@@ -1,36 +1,68 @@
-const btn = document.getElementById('login__button')
+/**
+ * Déclarations des variables
+ */
+const login_notification = document.getElementById('login_notification')
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const btnLoginSubmit = document.getElementById('login__button')
 
-btn.addEventListener('click', () => {
-    event.preventDefault();
-
-
-const email = document.getElementById('email').value;
-const password = document.getElementById('password').value;
-
-    fetch("http://localhost:5678/api/users/login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify ({"email": email,"password": password})
-
-    
-})
-
-.then((responseApi) => responseApi.json())
-.then((dataApi) => {
+/**
+* Connexion à la page principale en mode édition
+*/
+function displayOnEditpage (dataApi) {
     const loginToken = dataApi['token'];
+        if (loginToken!== undefined) {
+            localStorage.setItem("loginToken", loginToken);
+            setTimeout(() => {
+                window.location.replace("./index.html")
+            }, 1000);
+        }
+        else {
+            window.localStorage.removeItem('loginToken')
+            notification_error = document.createElement('p');
+            notification_error.classList.add('notification-error_login');
+            notification_error.innerHTML=`Erreur dans l’identifiant ou le mot de passe`;
 
-    window.localStorage.setItem("loginToken", loginToken);
-    setTimeout(() => {
-        window.location.replace("./index.html")
-    }, 1000);
+            login_notification.appendChild(notification_error)
+            window.localStorage.removeItem('loginToken')
+        }
+}
+
+/**
+* Connexion login dans l'API
+*/
+function getLogin () {
+    fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify ({
+            "email": email.value,
+            "password": password.value
+        })  
+    })
+    .then((responseApi) => responseApi.json())
+    .then((dataApi) => {
+        displayOnEditpage (dataApi)
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+}
+
+/**
+* Vérification des données de connexion au click du bouton "se connecter"
+*/
+btnLoginSubmit.addEventListener('click', (event) => {
+    event.preventDefault();
+    getLogin()
 })
-.catch((error)=> {
-    console.log(error)
-})
-
-})
-
-
     
-
-  
+/**
+* On retire le message d'erreur lors d'une modification des données email ou password
+*/
+email.addEventListener('input', () => {
+    login_notification.innerHTML=""
+})
+password.addEventListener('input', () => {
+    login_notification.innerHTML=""
+})
